821bf6dd510ff4a377bf13bb8b75e59f27cf10c1
Paul Gardner-Stephen <paul@m-e-g-a.org>
2023-10-01 16:43:52 +1030

    more work debugging IEC #736

--- src/tests/iecscan.c
+++ src/tests/iecscan.c
@@ -19,20 +19,30 @@
 #include <poll.h>
 #include <termios.h>
 
-#define PNG_DEBUG 3
-#include <png.h>
+#include <libpng16/png.h>
 
 int serialfd=-1;
 
+unsigned char dbg_vals[4096]={0};
+unsigned char dbg_states[4096]={0};
+
 char *sigs[5][24]={
   {"                        ",
-   "  xxxx   xxxxxx xx   xx ",
-   " xxxxxx    xx   xxx  xx ",
-   "xx    xx   xx   xxxx xx ",
-   "xxxxxxxx   xx   xx xxxx ",
-   "xx    xx   xx   xx  xxx ",
-   "xx    xx   xx   xx   xx ",
-   "xx    xx   xx   xx   xx "},
+   "xxxxxx   xxxxx  xxxxxx  ",
+   "xx   xx xx        xx    ",
+   "xx   xx xx        xx    ",
+   "xxxxxx   xxxxx    xx    ",
+   "xx xx        xx   xx    ",
+   "xx  xx       xx   xx    ",
+   "xx   xx  xxxxx    xx    "},
+  {"                        ",
+   "  xxx   xxxxxx xx   xx ",
+   " xxxxx    xx   xxx  xx ",
+   "xx   xx   xx   xxxx xx ",
+   "xxxxxxx   xx   xx xxxx ",
+   "xx   xx   xx   xx  xxx ",
+   "xx   xx   xx   xx   xx ",
+   "xx   xx   xx   xx   xx "},
   {"                        ",
    " xxxxx  xx      xx  xx  ",
    "xx   xx xx      xx xx   ",
@@ -42,13 +52,13 @@ char *sigs[5][24]={
    "xx   xx xx      xx  xx  ",
    " xxxxx  xxxxxxx xx  xx  "},
   {"                        ",
-   "xxxxxx  xxxxxx   xxxx   ",
-   "xx   xx   xx    xxxxxx  ",
-   "xx   xx   xx   xx    xx ",
-   "xx   xx   xx   xxxxxxxx ",
-   "xx   xx   xx   xx    xx ",
-   "xx   xx   xx   xx    xx ",
-   "xxxxxx    xx   xx    xx "},
+   "xxxxxx  xxxxxx   xxx   ",
+   "xx   xx   xx    xxxxx  ",
+   "xx   xx   xx   xx   xx ",
+   "xx   xx   xx   xxxxxxx ",
+   "xx   xx   xx   xx   xx ",
+   "xx   xx   xx   xx   xx ",
+   "xxxxxx    xx   xx   xx "},
   {"                        ",
    " xxxxx  xxxxxx   xxxxx  ",
    "xx      xx   xx xx   xx ",
@@ -56,15 +66,7 @@ char *sigs[5][24]={
    " xxxxx  xxxxxx  xx   xx ",
    "     xx xx xx   xx  x x ",
    "     xx xx  xx  xx  xxx ",
-   " xxxxx  xx   xx  xxxxxxx"},
-  {"                        ",
-   "xxxxxx   xxxxx  xxxxxx  ",
-   "xx   xx xx        xx    ",
-   "xx   xx xx        xx    ",
-   "xxxxxx   xxxxx    xx    ",
-   "xx xx        xx   xx    ",
-   "xx  xx       xx   xx    ",
-   "xx   xx  xxxxx    xx    "}
+   " xxxxx  xx   xx  xxxxxxx"}
 };
 
 char *digits[10][8]={
@@ -84,7 +86,7 @@ char *digits[10][8]={
    "   xx   ",
    "   xx   ",
    " xxxxxx "},
-  {"        "
+  {"        ",
    "  xxxx  ",
    " xx  xx ",
    "    xx  ",
@@ -150,43 +152,170 @@ char *digits[10][8]={
    " xxxxx  "}
 };
 
+#define MAXX 320
+#define MAXY 2048
+
+unsigned int pixels[MAXY][MAXX]={0};
+
+/*
+  Our waveform displays use 8x8 blocks to show each signal,
+  and the simple 8x8 font elements defined above.
+
+  Each row has room for 1020 ~1usec ticks, and is 40 pixels 
+  tall.  Thus for all 4096 ticks, we need four such rows. 
+ */
+void build_image(void)
+{
+  // Clear image to white initially
+  for(int y=0;y<MAXY;y++)
+    for(int x=0;x<MAXX;x++)
+      pixels[y][x]=0xffffffff;
+  
+  // Draw signal legend down the left side
+  for(int row = 0; ((row+1)*(9*8)) <= MAXY; row++) {
+    for(int sig = 0; sig<5; sig++) {
+      for (int charrow=0;charrow<8;charrow++) {
+	char *bits=sigs[sig][charrow];
+	for(int x=0;bits[x];x++) if (bits[x]!=' ') pixels[row*(9*8)+sig*8+charrow][x]=0xff000000;
+      }
+    }
+  }
+  
+  int x=32;
+  int y=0;
+
+  for(int n=0;n<4096;n++) {
+
+    // Draw state numbers under cells
+
+    if (dbg_states[n]) {
+      char num[16];
+      snprintf(num,16,"%d",dbg_states[n]);
+      int yy=y+5*8;
+      for(int c=0;num[c];c++) {
+	for(int charrow=0;charrow<8;charrow++) {
+	  int d=num[c]-'0';
+	  if (d>=0&&d<10) {
+	    char *r=digits[d][charrow];
+	    for(int col=0;col<8;col++) {
+	      int pixel=0;
+	      if (r[col]==' ') pixel=1; else pixel=0;
+	      pixels[yy+col][x+7-charrow]
+		=pixel?0xffffffff:0xff000000;
+	    }
+	  }
+	}
+	yy+=8;
+      }
+    }
+    
+    // Draw signal states
+    for(int sig=0;sig<5;sig++) {
+      // RST, ATN, CLK, DATA, SRQ
+
+      int controller=5;
+      int device=5;
+
+      int v=dbg_vals[n]^0xc0;
+      
+      switch(sig) {
+      case 0: // RST
+	controller=(v&0x80);
+	device=5;
+	break;
+      case 1: // ATN
+	controller=(v&0x40);
+	device=5;
+	break;
+      case 2: // CLK
+	controller=(v&0x10);
+	device=v&0x02;
+	break;
+      case 3: // DATA
+	controller=v&0x08;
+	device=v&0x01;
+	break;
+      case 4: // SRQ
+	controller=v&0x20;
+	device=v&0x04;
+	break;
+      }
+
+      int colour = 0x000000;
+      int voltage = 5;
+      
+      if (!device) {
+	// Device pulling low
+	colour = 0xff0000; // BLUE
+	voltage=0;
+      } else if (!controller) {
+	// Controller pulling low
+	colour = 0x0000ff; // RED
+	voltage=0;
+      } else {
+	// Neither pulling low
+	colour = 0x000000; // BLACK
+	voltage=5;
+      }
+
+      // Draw colour to indicate who is pulling low
+      for(int xx=0;xx<8;xx++) for(int yy=0;yy<7;yy++) pixels[y+sig*8+yy][x+xx]=0xff000000+colour;
+      for(int xx=0;xx<8;xx++) pixels[y+sig*8+7][x+xx]=0xff000000;
+
+      // Draw line at top or bottom
+      if (voltage) {
+	for(int xx=0;xx<8;xx++) pixels[y+sig*8+0][x+xx]=0xff00ffff; // YELLOW
+	for(int xx=0;xx<8;xx++) pixels[y+sig*8+1][x+xx]=0xff00ffff; // YELLOW
+      } else {
+	for(int xx=0;xx<8;xx++) pixels[y+sig*8+5][x+xx]=0xff00ffff; // YELLOW
+	for(int xx=0;xx<8;xx++) pixels[y+sig*8+6][x+xx]=0xff00ffff; // YELLOW
+      }
+    }
+
+    x+=8;
+    if (x>=MAXX) {
+      x=32; y+=(9*8);
+    }
+    if (y>(MAXY-(9*8)+1)) break;
+  }
+}
+
 void write_png(char *filename)
 {
   int y;
   png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if (!png)
+  if (!png) {
+    fprintf(stderr,"FATAL: png_create_write_struct() failed\n");
     abort();
+  }
 
   png_infop info = png_create_info_struct(png);
-  if (!info)
+  if (!info) {
+    fprintf(stderr,"FATAL: png_create_info_struct() failed\n");
     abort();
+  }
 
-  if (setjmp(png_jmpbuf(png)))
+  if (setjmp(png_jmpbuf(png))) {
+    fprintf(stderr,"FATAL: png_jmpbuf() failed\n");
     abort();
+  }
 
   FILE *f = fopen(filename, "wb");
-  if (!f)
+  if (!f) {
+    fprintf(stderr,"FATAL: Failed to open '%s' for write\n",filename);
     abort();
+  }
 
   png_init_io(png, f);
 
-  int MAXX = 1024;
-  int MAXY = 768;
-
-  unsigned int image[MAXY][MAXX];
-  
   png_set_IHDR(
       png, info, MAXX, MAXY, 8, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_DEFAULT);
 
   png_write_info(png, info);
 
-#if 0
-  for (y = 0; y < maxy; y++) {
-    printf("  writing y=%d\n", y);
-    fflush(stdout);
-    png_write_row(png, frame[y]);
+  for (y = 0; y < MAXY; y++) {
+    png_write_row(png, (unsigned char *)pixels[y]);
   }
-#endif
   
   png_write_end(png, info);
   png_destroy_write_struct(&png, &info);
@@ -306,22 +435,32 @@ void iecReset(void)
   writeReg(REG_CMD,0x52);
   getUpdate();
   // Wait until all lines are released by drive
-  while(iec_status&0x2c!=0x2c) {
+  while((iec_status&0x2c)!=0x2c) {
     usleep(100000);
     getUpdate();
   }
 }
 
-iecDataTrace(char *msg)
+int iecDataTrace(char *msg)
 {
   fprintf(stderr,"DEBUG: Fetching IEC data trace...\n");
   writeReg(REG_DBG,0x00); // Reset data pointer to start of buffer
   for(int i=0;i<4096;i++) {
     getUpdate();
     printf(" $%02x/%d",iec_debug_ram,iec_debug_ram2); fflush(stdout);
+    dbg_vals[i]=iec_debug_ram;
+    dbg_states[i]=iec_debug_ram2;
+
+    if(i&&(!iec_debug_ram2)) break;
+    
     writeReg(REG_DBG,0x01); // Advance to next value in debug trace buffer
   }
+    
+  build_image();
+  write_png("iectrace.png");
+
   printf("\n");
+  return 0;
 }
 
 int main(int argc,char **argv)
