21e3df73337d08960d93274644ed42c34346829f
Paul Gardner-Stephen <paul@m-e-g-a.org>
2023-10-26 13:16:02 +1030

    fix phase advancing #736

--- src/vhdl/m6522.vhdl
+++ src/vhdl/m6522.vhdl
@@ -211,7 +211,7 @@ begin
         -- nothing uses that phase to do stuff. All other phases
         -- DO have things attached to them.
         if phase /= "10" then
-          phase <= phase + "1";
+          phase <= std_logic_vector(to_unsigned(to_integer(unsigned(phase)) + 1,2));
         end if;
       end if;
     end if;
@@ -643,11 +643,14 @@ begin
   begin
     wait until rising_edge(CLK);
     if (ENA_4 = '1') then
+      report "MOS6522: Phase = " & to_string(phase);
       if t1_load_counter or (t1_reload_counter and phase = "11") then
+        report "MOS6522: Load counter 1";
         t1c( 7 downto 0) <= r_t1l_l;
         t1c(15 downto 8) <= r_t1l_h;
       elsif (phase="11") then
         t1c <= t1c - "1";
+        report "MOS6522: Timer 1 tick. Counter = " & to_hexstring(t1c) & " - 1";
       end if;
 
       if t1_load_counter or t1_reload_counter then
