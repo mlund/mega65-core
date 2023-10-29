09934467fdba08fc7e0be353c4b2ae20824b0e75
Paul Gardner-Stephen <paul@m-e-g-a.org>
2023-10-29 20:48:48 +1030

    extend delays in turnaround #736

--- src/vhdl/iec_serial.vhdl
+++ src/vhdl/iec_serial.vhdl
@@ -846,9 +846,9 @@ begin
             -- Computer pulls DATA low and releases CLK.
             -- Device then pulls CLK low and releases DATA.
             
-          when 200 => micro_wait(20);
-          when 201 => a('1'); micro_wait(20);
-          when 202 => d('0'); c('1'); micro_wait(20);
+          when 200 => micro_wait(100);
+          when 201 => a('1'); micro_wait(100);
+          when 202 => d('0'); c('1'); micro_wait(100);
           when 203 => milli_wait(64); wait_clk_low <= '1';
           when 204 =>
             if iec_clk_i = '0' then
